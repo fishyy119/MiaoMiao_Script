a1eb452975e4d1e551f68ac9745d01340fb251cb
fishyy119 <1830979240@qq.com>
2024-03-06 13:34:17 +0800

    修复匹配算法漏洞

--- MatchStr.py
+++ MatchStr.py
@@ -47,6 +47,7 @@ class WordMatcher:
         # 查询方式：match_matrix[S_index][L_index]
         # 歌词L为主动方，歌曲S为被动方
 
+        match_matrix_T = list(zip(*self.match_matrix))
         L_prefs = {L: list(range(len(self.list1))) for L in range(len(self.list2))}  # 用于记录L的待匹配项
         free_L = list(range(len(self.list2)))  
         match_dict = {}  # 顺序S: L
@@ -56,7 +57,8 @@ class WordMatcher:
             if len(L_prefs[L]) == 0:  # 已经配过过所有歌曲的L:不再进行匹配，相当于被移出free_L
                 continue
 
-            S = L_prefs[L].pop(0)
+            first_S_index = match_matrix_T[L].index(max(match_matrix_T[L]))  # 取出最优的歌曲
+            S = L_prefs[L].pop(first_S_index)
             if self.match_matrix[S][L] != 0:  # 忽略相似度为0项
                 if S not in match_dict:  # S未匹配，则进行配对
                     match_dict[S] = L
