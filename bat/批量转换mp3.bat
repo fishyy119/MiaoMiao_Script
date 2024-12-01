75df9a74ed99d75a7d71300b30171d27827053ce
fishyy119 <1830979240@qq.com>
2024-12-01 11:07:57 +0800

    "bat/mp3转换": 修复文件名中叹号导致的失效

--- bat/批量转换mp3.bat
+++ bat/批量转换mp3.bat
@@ -1,13 +1,18 @@
 @echo off
-setlocal enabledelayedexpansion
 
 :: 创建保存mp3文件的目录
 mkdir mp3_files
 
 :: 遍历当前目录下的所有wav和flac文件
 for /r %%d in (*.wav *.flac) do (
-    echo Converting: %%~nd
-    ffmpeg -i "%%d" -codec:a libmp3lame -b:a 320k -ar 44100 -ac 2 -loglevel error "mp3_files/%%~nd.mp3"
+
+    set f=%%d
+    set fn=%%~nd
+    :: 开启变量延迟后，文件名中叹号会被去掉，所以先获取文件名再开启变量延迟
+    setlocal enabledelayedexpansion
+
+    echo Converting: !f!
+    ffmpeg -i "!f!" -codec:a libmp3lame -b:a 320k -ar 44100 -ac 2 -loglevel error "mp3_files/!fn!.mp3"
 )
 
 echo All files have been processed.
